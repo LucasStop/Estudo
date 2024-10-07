339d61a749aeef83c29db22ba94baf29f3bdf422
Lucas Stopinski da Silva <lucasstopinskidasilva@gmail.com>
2024-10-07 09:23:01 -0300

    Primeiros Passos com React
    Banner principal da página da organo

---
+++ organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/imgs/banner.png" alt="Banner Principal da Página da Organo" />
+        </header>
+    )
+}
+
+document.createElement('img')
+
+export default Banner
