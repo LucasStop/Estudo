dc53ee40f856014ace71218a5d0033305dc817ec
Lucas Stopinski da Silva <lucasstopinskidasilva@gmail.com>
2024-10-07 13:38:08 -0300

    Criação do Campo de Texto

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,9 +1,14 @@
-import Banner from './componentes/Banner/Banner';
+import Banner from './componentes/Banner';
+import CampoTexto from './componentes/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto label="Nome" />
+      <CampoTexto label="cargo" />
+      <CampoTexto label="Imagem" />
+      <CampoTexto label="Nome" />
     </div>
   );
 }
--- organo/src/componentes/Banner/Banner.js
+++ organo/src/componentes/Banner/Banner.js
@@ -1,13 +1,9 @@
 import './Banner.css'
 
-function Banner() {
+export const Banner = () => {
     return (
         <header className="banner">
             <img src="/imgs/banner.png" alt="Banner Principal da Página da Organo" />
         </header>
     )
 }
-
-document.createElement('img')
-
-export default Banner
