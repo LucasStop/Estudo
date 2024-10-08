1ee79e98251edf28f01d68e65291a4e12d084d5a
Lucas Stopinski da Silva <lucasstopinskidasilva@gmail.com>
2024-10-08 16:10:24 -0300

    Obrigatoriedade de Campos
    Usando o HTML5

--- organo/src/componentes/ListaSuspensa/index.js
+++ organo/src/componentes/ListaSuspensa/index.js
@@ -5,7 +5,7 @@ const ListaSuspensa = (props) => {
     return (
         <div className="lista-suspensa">
             <label>{props.label}</label>
-            <select>
+            <select required={props.obrigatorio}>
                 {props.itens.map((item, index) => {
                     return <option key={index}>{item}</option>
                 })}
