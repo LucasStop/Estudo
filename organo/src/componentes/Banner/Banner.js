66ad8bb0a6d739825ec5f1e5f2eef0cb26d52772
Lucas Stopinski da Silva <lucasstopinskidasilva@gmail.com>
2024-10-16 11:03:30 -0300

    Manipulando o input do usuário

--- organo/src/componentes/Banner/Banner.js
+++ organo/src/componentes/Banner/Banner.js
@@ -1,9 +1,10 @@
 import './Banner.css'
 
 export const Banner = () => {
+    // JSX
     return (
         <header className="banner">
-            <img src="/imgs/banner.png" alt="Banner Principal da Página da Organo" />
+            <img src="/imgs/banner.png" alt="O banner principal da página do Organo"/>
         </header>
     )
 }
